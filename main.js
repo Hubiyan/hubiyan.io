5e428a5bd1e2dd37a57766380042d5a5b138e062
Hubiyan <hubiyan.work@gmail.com>
2022-11-23 02:24:32 +0530

    new images and sections

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
   $(".radio-btn").click(function () {
     $(".radio-inner").toggleClass("active");
     $("*").toggleClass("dark");
+    localStorage.getItem('dark-mode', 'dark');
   });
 });
+
